831b62805ea387882682e5eff67d8c20cd3db63f
Kenneth Lee <kennethlhc@gmail.com>
2019-08-19 12:57:58 +0800

    Level-2, Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,20 +6,32 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
 
-    private static void Echo(String a) {
-        System.out.println(a);
-    }
-
     private static void Exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void Store(ArrayList<String> a, String b) {
+        a.add(b);
+        System.out.println("added: " + b);
+    }
+
+    private static void Echo(ArrayList<String> a) {
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(i+1 + ". " + a.get(i));
+        }
     }
 
     public static void main(String[] args) {
         Greet();
         Scanner obj = new Scanner(System.in);
         String input;
+        ArrayList<String> list = new ArrayList<String>();
         while (!(input = obj.nextLine()).equals("bye")) {
-            Echo(input);
+            if (!input.equals("list")) {
+                Store(list, input);
+            } else {
+                Echo(list);
+            }
         }
         Exit();
     }
