25b38d4f99bac5b155553403d576523ffdc9fe13
Kenneth Lee <kennethlhc@gmail.com>
2019-08-21 14:44:55 +0800

    Level-3, Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,38 +2,64 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static void Greet() {
+    private static ArrayList<Task> list = new ArrayList<>();
+
+    private static void greet() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
 
-    private static void Exit() {
+    private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private static void Store(ArrayList<String> a, String b) {
-        a.add(b);
-        System.out.println("added: " + b);
+    private static void store(Task a) {
+        list.add(a);
+        System.out.println("added: " + a.description);
     }
 
-    private static void Echo(ArrayList<String> a) {
-        for (int i = 0; i < a.size(); i++) {
-            System.out.println(i+1 + ". " + a.get(i));
+    private static void echo() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i+1 + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
         }
+    }
+
+    private static void taskComplete(int i) {
+        list.get(i).markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
+    }
+
+    private static void removeTask(int i) {
+        System.out.println("Okay! I've removed this task:");
+        System.out.println("[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
+        list.remove(i);
     }
 
     public static void main(String[] args) {
-        Greet();
+        greet();
         Scanner obj = new Scanner(System.in);
         String input;
-        ArrayList<String> list = new ArrayList<String>();
         while (!(input = obj.nextLine()).equals("bye")) {
-            if (!input.equals("list")) {
-                Store(list, input);
+            if (input.equals("list")) {
+                echo();
+            } else if (input.isEmpty()) {
+              System.out.println("You did not enter a task, what can I do for you?");
             } else {
-                Echo(list);
+                String[] words = input.split(" ", 2);
+                if (words[0].equals("done") || words[0].equals("Done")) {
+                    int check = Integer.parseInt(words[1]) - 1;
+                    taskComplete(check);
+                } else if (words[0].equals("remove") || words[0].equals("Remove")) {
+                    int check = Integer.parseInt(words[1]) - 1;
+                    removeTask(check);
+                } else {
+                    Task task = new Task(input);
+                    store(task);
+                }
             }
         }
-        Exit();
+        exit();
     }
 
 }
