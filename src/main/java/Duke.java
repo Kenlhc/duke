4ba450d44da79ab1871c2c0dbf0f960d03f71f6a
Kenneth Lee <kennethlhc@gmail.com>
2019-08-24 14:48:04 +0800

    Revert "Level-4, A-Inheritance"
    This reverts commit ce44918a

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,33 +14,25 @@ public class Duke {
 
     private static void store(Task a) {
         list.add(a);
-        if (list.size() == 1) {
-            System.out.println("Got it. I've added this task:\n"
-                    + "   " + a.toString()
-                    + "\nNow you have " + list.size() + " task in the list");
-        } else {
-            System.out.println("Got it. I've added this task:\n"
-                    + "   " + a.toString()
-                    + "\nNow you have " + list.size() + " tasks in the list");
-        }
+        System.out.println("added: " + a.description);
     }
 
     private static void echo() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(i+1 + "." + list.get(i).toString());
+            System.out.println(i+1 + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
         }
     }
 
     private static void taskComplete(int i) {
         list.get(i).markAsDone();
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println(list.get(i).toString());
+        System.out.println("[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
     }
 
     private static void removeTask(int i) {
         System.out.println("Okay! I've removed this task:");
-        System.out.println(list.get(i).toString());
+        System.out.println("[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
         list.remove(i);
     }
 
@@ -61,16 +53,8 @@ public class Duke {
                 } else if (words[0].equals("remove") || words[0].equals("Remove")) {
                     int check = Integer.parseInt(words[1]) - 1;
                     removeTask(check);
-                } else if (words[0].equals("todo")) {
-                    Task task = new Todo(words[1]);
-                    store(task);
-                } else if (words[0].equals("deadline")) {
-                    String[] splitter = words[1].split("/by");
-                    Task task = new Deadline(splitter[0].trim(), splitter[1].trim());
-                    store(task);
-                } else if (words[0].equals("event")) {
-                    String[] splitter = words[1].split("/at");
-                    Task task = new Event(splitter[0].trim(), splitter[1].trim());
+                } else {
+                    Task task = new Task(input);
                     store(task);
                 }
             }
